5d69c8880291883b53445fb2dd0131180af3bd6e
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-25 10:46:58 +0530

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -1,3 +1,4 @@
-token = ""  # telegram token
-logs = 2390049  # logs channel id, if none set to None
+token = "6999401413:AAHgF1ZpUsCT5MgWX1Wky7GbegyeHvzi2AU" 
+logs = None
 max_filesize = 50000000  # bytes
+
